d921ab1437624af4b7f5e490ff34d5200953eb8f
papamitra <papamitra@gmail.com>
2017-01-14 14:37:38 +0900

    3.1

--- web/router.ex
+++ web/router.ex
@@ -16,7 +16,7 @@ defmodule SampleApp.Router do
   scope "/", SampleApp do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
+    get "/hello", PageController, :index
   end
 
   # Other scopes may use custom stacks.
