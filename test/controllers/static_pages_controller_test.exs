8f69a29087ee0c9010f19927093047f804e6ac5e
papamitra <papamitra@gmail.com>
2017-01-17 22:53:40 +0900

    3.4.2 タイトルを追加する

--- test/controllers/static_pages_controller_test.exs
+++ test/controllers/static_pages_controller_test.exs
@@ -1,18 +1,24 @@
 defmodule SampleApp.StaticPagesControllerTest do
   use SampleApp.ConnCase
 
+  @base_title "Ruby on Rails Tutorial Sample App"
+
+  defp get_title(conn) do
+    Floki.find(html_response(conn, 200), "title") |> Enum.at(0) |> elem(2) |> Enum.at(0)
+  end
+
   test "should get home", %{conn: conn} do
     conn = get conn, static_pages_path(conn, :home)
-    assert html_response(conn, 200)
+    assert "Home | #{@base_title}" == get_title(conn)
   end
 
   test "should get help", %{conn: conn} do
     conn = get conn, static_pages_path(conn, :help)
-    assert html_response(conn, 200)
+    assert "Help | #{@base_title}" == get_title(conn)
   end
 
   test "should get about", %{conn: conn} do
     conn = get conn, static_pages_path(conn, :about)
-    assert html_response(conn, 200)
+    assert "About | #{@base_title}" == get_title(conn)
   end
 end
--- web/controllers/static_pages_controller.ex
+++ web/controllers/static_pages_controller.ex
@@ -2,15 +2,15 @@ defmodule SampleApp.StaticPagesController do
   use SampleApp.Web, :controller
 
   def home(conn, _params) do
-    render conn, "home.html"
+    render conn, "home.html", layout: false
   end
 
   def help(conn, _params) do
-    render conn, "help.html"
+    render conn, "help.html", layout: false
   end
 
   def about(conn, _params) do
-    render conn, "about.html"
+    render conn, "about.html", layout: false
   end
 
 end
