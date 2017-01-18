804f320e14bb0c15dee6a9996a92ccb35169513c
papamitra <papamitra@gmail.com>
2017-01-18 21:22:57 +0900

    3.4.3 レイアウトと埋め込みRuby

--- web/controllers/static_pages_controller.ex
+++ web/controllers/static_pages_controller.ex
@@ -2,15 +2,15 @@ defmodule SampleApp.StaticPagesController do
   use SampleApp.Web, :controller
 
   def home(conn, _params) do
-    render conn, "home.html", layout: false
+    render conn, "home.html", title: "Home"
   end
 
   def help(conn, _params) do
-    render conn, "help.html", layout: false
+    render conn, "help.html", title: "Help"
   end
 
   def about(conn, _params) do
-    render conn, "about.html", layout: false
+    render conn, "about.html", title: "About"
   end
 
 end
