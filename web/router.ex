8a826f91c2b19f215c1846ab32fe40761085432f
papamitra <papamitra@gmail.com>
2017-01-25 16:53:06 +0900

    5.3.2 RailsのルートURL

--- web/router.ex
+++ web/router.ex
@@ -17,10 +17,10 @@ defmodule SampleApp.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", StaticPagesController, :home
-    get "/static_pages/home", StaticPagesController, :home
-    get "/static_pages/help", StaticPagesController, :help
-    get "/static_pages/about", StaticPagesController, :about
-    get "/static_pages/contact", StaticPagesController, :contact
+    get "/home", StaticPagesController, :home
+    get "/help", StaticPagesController, :help
+    get "/about", StaticPagesController, :about
+    get "/contact", StaticPagesController, :contact
   end
 
   # Other scopes may use custom stacks.
